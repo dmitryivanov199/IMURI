9ac3d6bc00ed9622224710291d3dae777821ac5a
dmitryivanov199 <dmitryivanov199@mail.ru>
2022-01-06 14:49:11 +0300

    Remove unused RAP-part and tests

--- main.cpp
+++ main.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
 
-//#include "CppUTest/CommandLineTestRunner.h"
-
 int main(int argc, char *argv[]) {
-//    return CommandLineTestRunner::RunAllTests(argc, argv);
-
     return 0;
 }
